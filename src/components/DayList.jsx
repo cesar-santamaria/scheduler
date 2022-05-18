779ad59179259f1f1927fbaccb827edc035ff44b
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-18 17:17:02 -0500

    add interviewList component and styling

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DayListItem from './DayListItem.jsx'
 
 export default function DayList(props) {
-  const dayListItem = props.days.map((day) => 
+  const dayListItemArray = props.days.map((day) => 
     <DayListItem 
       key={day.id} 
       name={day.name} 
@@ -12,6 +12,6 @@ export default function DayList(props) {
     />
   ) 
   return (
-    <ul>{dayListItem}</ul>
+    <ul>{dayListItemArray}</ul>
   )
 }
