ff07935d41c83e8d67c39dae75857e8109d29641
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-17 19:23:37 -0500

    add styles to DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import "../styles/DayListItem.scss"
+import classNames from 'classnames';
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+    }
+  );
+
   return (
-    <li onClick={()=>props.setDay(props.name)}>
+    <li className={dayClass} onClick={()=>props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
+
+/* 
+We'll need to update our component to apply the day-list__item--selected and day-list__item--full styles based on the props we are passing to DayListItem using the classnames library, as we did in the Button component in an earlier activity.
+*/
