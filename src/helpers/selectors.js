bc2f7052e5e3c68aa3a2b380c6a31b6ba9e7f44c
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-22 20:17:12 -0500

    add getInterview selector function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,24 @@
 export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter(days => days.name === day);
-
   if (!filteredDays[0]) {
     return [];
   } else {
     const appointmentId = filteredDays[0].appointments.map((appointmentId) => state.appointments[appointmentId]);
     return appointmentId
   }
+};
+
+export function getInterview(state, interview) {
+  let filteredInterview = {...interview};
+  
+  if(!interview) {
+    return null;
+  }
+
+  for (const interviewerId in state.interviewers) {
+    if (Number(interviewerId) === Number(interview.interviewer)) {
+      filteredInterview.interviewer = state.interviewers[interviewerId]
+    }
+  }
+  return filteredInterview;
 };
